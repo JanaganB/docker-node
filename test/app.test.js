3d27298c581755b01bbccee20fa932b7159d8a16
Marco Biedermann <marco.biedermann@gmx.de>
2018-11-09 08:35:10 +0100

    feat: Tests
    Add supertest HTTP assertion tests

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../server/app');
+
+describe('GET /', () => {
+  it('respond with text', done => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /text\/html/)
+      .expect(200, done);
+  });
+});
