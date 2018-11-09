e91030866dc0fcbb64ae9dc7e1d386b99bc7ce5d
Marco Biedermann <marco.biedermann@gmx.de>
2018-11-09 08:38:39 +0100

    chore: ESLint
    Add ESLint

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../server/app');
 
 describe('GET /', () => {
-  it('respond with text', done => {
+  it('respond with text', (done) => {
     request(app)
       .get('/')
       .expect('Content-Type', /text\/html/)
